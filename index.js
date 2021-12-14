0eaf3bed544a97e544f3f042e3ee126f1a0159de
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-12-14 13:16:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.Client = require("./lib/Client");
+module.exports.Database = require("./lib/Database");
+module.exports.Utils = require("./lib/Utils");
+module.exports.API = require("./lib/Api")
